932190684badcf34ca8dedad8bf902d175d3aea3
Pravallika9929 <121336879+Pravallika9929@users.noreply.github.com>
2023-10-29 14:23:12 +0530

    Update lab9.py

--- id_2100080068/lab9.py
+++ id_2100080068/lab9.py
@@ -21,3 +21,22 @@ epochs 10000
 for epoch in range(epochs): #Forward propagation
 hidden input = np.dot (X, weights_input_hidden) + bias_hidden hidden_output = sigmoid(hidden_input)
 output_layer_input = np.dot (hidden_output, weights_hidden_output) bias_output output layer output sigmoid (output_layer_input)
+output_layer_output = sigmoid(output_layer_input)
+loss = y - output_layer_output
+# Backpropagation
+d_output = loss * sigmoid_derivative(output_layer_output)
+loss_hidden = d_output. dot(weights_hidden_output.T)
+d_hidden = loss_hidden * sigmoid_derivative(hidden_output)
+# Update the weights and biases
+weights_hidden_output += hidden_output. T.dot(d_output) * learning_rate weights_input_hidden += X.T.dot (d hidden) * learning_rate bias_output += np. sumd_output, axis=0, keepdims=True) * learning_rate bias_hidden += np. sumd_hidden, axis=0, keepdims=True) * learning_rate
+# Testing the neural network
+# Print the final output
+print ("Final Output:")
+print(output_layer_output)
+# Print the final weights and biases
+print("\nFinal Weights and Biases:")
+print ("Weights Input to Hidden Layer:") printmeats. put hi Layer:"
+print (bias_hidden)
+print("Weights Hidden to Output Layer:")
+print (weights_hidden_output)
+print is up Output Layer:")
